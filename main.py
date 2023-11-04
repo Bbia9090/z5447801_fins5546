5e2e754b4233c9fd36d91b4d27fbb6979e77860b
biying.qin <biying.qin@biyingqindeMacBook-Pro.local>
2023-11-05 02:31:49 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Downloads Qantas share price beginning 1 January 2020
+import yfinance                                           # (1)
+tic = "QAN.AX"                                            # (2)
+start = '2020-01-01'                                      # (3)
+end = None                                                # (4)
+df = yfinance.download(tic, start, end, ignore_tz=True)   # (5)
+print(df)                                                 # (6)
+df.to_csv('qan_stk_prc.csv')                              # (7)# Downloads Qantas share price beginning 1 January 2020
+
+
+# Try it!
+tic = QAN.AX
+
